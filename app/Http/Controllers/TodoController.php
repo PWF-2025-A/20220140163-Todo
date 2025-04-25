9146cf9dd6abe6d2645811dfa03f7fb8f2d2b0d8
DimasCW <cdimas292@gmail.com>
2025-04-25 18:45:17 +0700

    meyesuaikan TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,8 +11,8 @@ class TodoController extends Controller
     //
     public function index()
     {
-        $todos = Todo::where('user_id', Auth::id())->get();
-        return view('todo.index');
+        $todos = Todo::where('user_id', Auth::id())->orderBy('created_at', 'desc')->get();
+        return view('todo.index', compact('todos'));
     }
 
     public function create()
@@ -24,4 +24,16 @@ class TodoController extends Controller
     {
         return view('todo.edit');
     }
-}
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => Auth::id(),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully.');
+}}
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,21 +1,28 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Todo') }}
-        </h2>
+<div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+    <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
+        <div class="flex items-center justify-between">
+            <div>
+                <x-create-button href="{{  route('todo.create') }}"/>
+            </div>
+            <div>
+                @if (session('success'))
+                <p x-data="{show:true}" x-show="show" x-trasition
+                x-init="setTimeout(() => show = false, 5000)"
+                class="text-sm text-green-600 dark:text-green-400">
+                    {{ session('success') }}            
+            </p>
 
-    </x-slot>
-
-
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{ __('Index Todo Page') }}
+            @endif
+            @if (session('danger'))
+            <p x-data="{show:true}" x-show="show" x-trasition
+                x-init="setTimeout(() => show = false, 5000)"
+                class="text-sm text-red-600 dark:text-red-400">
+                    {{ session('danger') }}
+            
+            @endif
             </div>
         </div>
 
     </div>
-</div>
 
-</x-app-layout>
+    
