d083e762af31a010f3b7e3a0f4a004dae9a68d24
DimasCW <cdimas292@gmail.com>
2025-05-03 00:16:19 +0700

    update, delete data

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,66 +3,111 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Todo') }}
         </h2>
-
     </x-slot>
 
-
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{ __('Index Todo Page') }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Index Todo Page') }}
+                </div>
             </div>
         </div>
+    </div>
+
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-6">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Title</th>
+                        <th scope="col" class="px-6 py-3">Status</th>
+                        <th scope="col" class="px-6 py-3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($todos as $data)
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                <a href="{{ route('todo.edit', $data) }}" class="hover:underline text-xs">
+                                    {{ $data->title }}
+                                </a>
+                            </td>
+                            <td class="px-6 py-4">
+                                @if (!$data->is_complete)
+                                    <span class="inline-flex items-center bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">
+                                        On Going
+                                    </span>
+                                @else
+                                    <span class="inline-flex items-center bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">
+                                        Done
+                                    </span>
+                                @endif
+                            </td>
+                            <td class="px-6 py-4">
+                                <div class="flex space-x-3">
+                                    @if (!$data->is_complete)
+                                        <form action="{{ route('todo.complete', $data) }}" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit"
+                                                class="text-xs text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg px-2.5 py-1.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                Complete
+                                            </button>
+                                        </form>
+                                    @else
+                                        <form action="{{ route('todo.uncomplete', $data) }}" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit"
+                                                class="text-xs text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg px-2.5 py-1.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                                Uncomplete
+                                            </button>
+                                        </form>
+                                    @endif
+
+                                    <form action="{{ route('todo.destroy',$todo) }}" method="Post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"
+                                            class=""text-red-600 dark:text-red-400>
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                            <td colspan="3" class="px-6 py-4 text-center text-gray-500 dark:text-gray-400">
+                                No data available
+                            </td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        @if ($todosCompleted > 1)
+            <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
+                <form action="{{ route('todo.deleteallcompleted') }}" method="Post">
+                    @csrf
+                    @method('delete')
+
+                    <x-primary-button>
+                        Delete All Completed Task
+                    </x-primary-button>
+                </form>
+            </div>
+        @endif
+        <form action="{{ route('user.destroy', $user) }}" method="Post">
+            @csrf
+            @method('delete')
+            <button type="submit"
+                class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                Delete
+            </button>
+        </form>
+
 
     </div>
-</div>
-
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
- <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
- <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray400">
- <thead class="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700
-dark:text-gray-400">
- <tr>
- <th scope="col" class="px-6 py-3">Title</th>
- <th scope="col" class="px-6 py-3">Status</th>
- <th scope="col" class="px-6 py-3">Action</th>
- </tr>
- </thead>
- <tbody>
- @forelse ($todos as $data)
- <tr
- class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bggray-800 border-b dark:border-gray-700 border-gray-200">
- <td scope="row" class="px-6 py-4 font-medium text-white dark:text-gray900">
- <a href="{{ route('todo.edit', $data) }}"
- class="hover:underline text-xs">{{ $data->title }}</a>
- </td>
- <td class="px-6 py-4 md:block">
- @if ($data->is_done == false)
- <span
- class="inline-flex items-center bg-red-100 text-red-800 text-sm
-font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">
- On Going
- </span>
- @elseif ($data->is_done == true)
- <span
- class="inline-flex items-center bg-green-100 text-green-800 textsm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">
- Done
- </span>
- @endif
- </td>
- </tr>
- @empty
- <tr
- class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bggray-800 border-b dark:border-gray-700 border-gray-200">
- <td colspan="3" class="px-6 py-4 text-center text-gray-500 dark:textgray-400">
- No data available
- </td>
- </tr>
- @endforelse
- </tbody>
- </table>
- </div>
- </div>
-
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,13 +20,24 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::post('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
-
-    Route::get(`/user`, [UserController::class, 'index'])->name('user.index');
+    // Todo Routes
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::resource('todo', TodoController::class)->except(['show']);
+
+    // User Routes
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+    Route::delete('/user/{user}',[UserController::class, 'destroy'])-> name('user.destroy');
+    Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
+
+    Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+
+    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])-> name('todo.destroy');
+    Route::delete('/todo',[TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+
 
 });
 
